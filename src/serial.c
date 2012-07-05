6630dc8af386290bb554ab0a928be70e0c57d1c1
Jussi Eloranta <eloranta@jussi.(none)>
2012-07-04 17:52:32 -0700

    Fixed Makefile and added 38400 baud rate to serial.c

--- src/serial.c
+++ src/serial.c
@@ -49,6 +49,9 @@ int meas_rs232_open(char *dev, int speed) {
   case MEAS_B57600:
     newtio.c_cflag = B57600 | CS8 | CLOCAL | handshake | CREAD;
     break;
+  case MEAS_B38400:
+    newtio.c_cflag = B38400 | CS8 | CLOCAL | handshake | CREAD;
+    break;
   case MEAS_B115200:
     newtio.c_cflag = B115200 | CS8 | CLOCAL | handshake | CREAD;
     break;
--- src/serial.h
+++ src/serial.h
@@ -1,7 +1,8 @@
 #define MEAS_B9600   0
 #define MEAS_B19200  1
 #define MEAS_B57600  2
-#define MEAS_B115200 3
+#define MEAS_B38400  3
+#define MEAS_B115200 4
 
 #define MEAS_NOHANDSHAKE 246
 
