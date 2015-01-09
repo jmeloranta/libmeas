ebd311e21329f48a592cbc24022be812a81b10b7
Jussi Eloranta <jmeloranta@gmail.com>
2015-01-08 16:13:36 -0800

    conf.h missing

---
+++ examples/csun/video/conf.h
@@ -0,0 +1,25 @@
+/* 
+ * CSUN Configuration.
+ *
+ */
+
+/*
+ * GPIB configuration.
+ *
+ */
+
+#define DG535  16
+#define BNC565 15
+
+/* Standard buffer size for reads */
+#define BUF_SIZE 4096
+
+/* Internal electronics delays for Surelite-II, Minilite-II (excl. Q-switch) and the ICCD in sec */
+
+#define MINILITE_DELAY (0.140E-6 - 10.025E-6)
+#define SURELITE_DELAY 0.380E-6
+
+/* Prototypes etc. */
+void surelite_qswitch(double), minilite_qswitch(double), laser_stop(), laser_start();
+double surelite_int_delay(double), minilite_int_delay(double);
+void surelite_delay(double), minilite_delay(double), laser_set_delays();
