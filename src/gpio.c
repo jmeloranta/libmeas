f55c434ac994c309d625b10f49c887f08e3d04b2
Jussi Eloranta <jmeloranta@gmail.com>
2019-08-07 16:15:45 -0700

    Simple pulse generator using raspberry pi.

--- src/gpio.c
+++ src/gpio.c
@@ -12,7 +12,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/mman.h>
---
+++ src/pulsegen.c
@@ -0,0 +1,84 @@
+/*
+ * Simple pulse generator using Raspberry Pi (using gpio.c).
+ *
+ * Channel to BCM GPIO pin map for Raspberry Pi 3 (use gpio readall to see):
+ *
+ * Channel		BCM pin #
+ * MEAS_PULSE_CH1	GPIO4 (7)
+ * MEAS_PULSE_CH2	GPIO5 (29)
+ * MEAS_PULSE_CH3	GPIO6 (31)
+ * MEAS_PULSE_CH4	GPIO12 (32)
+ * MEAS_PULSE_CH5	GPIO13 (33)
+ * MEAS_PULSE_CH6	GPIO16 (36)
+ * MEAS_PULSE_CH7	GPIO17 (11)
+ * MEAS_PULSE_CH8	GPIO18 (12)
+ * MEAS_PULSE_CH9	GPIO19 (35)
+ * MEAS_PULSE_CH10	GPIO20 (38)
+ * MEAS_PULSE_CH11	GPIO21 (40)
+ * MEAS_PULSE_CH12	GPIO22 (41)
+ * MEAS_PULSE_CH13	GPIO23 (16)
+ * MEAS_PULSE_CH14	GPIO24 (18)
+ * MEAS_PULSE_CH15	GPIO25 (22)
+ * MEAS_PULSE_CH16	GPIO26 (37)
+ * MEAS_PULSE_CH17	GPIO27 (13)
+ *
+ * Note: these are NOT wiringPi numberings.
+ * 
+ * It is useful to construct a box that has opto-isolators and proper BNC connectors for connecting cables.
+ *
+ * The time resolution is 1 microsecond +- 0.1 microsec.
+ *
+ */
+
+#ifdef RPI
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "pulsegen.h"
+#include "misc.h"
+
+/*
+ * Initialize the pulse generator.
+ *
+ */
+
+EXPORT int meas_pulse_open() {
+
+  return meas_gpio_open();  
+}
+
+/*
+ * Execute pulse sequence.
+ *
+ * pulses  = Array of pulse information (struct meas_pulse).
+ * npulses = Number of pulses in array (int).
+ *
+ * No return value.
+ *
+ */
+
+EXPORT void meas_pulse_exec(struct meas_pulse *pulses, int npulses) {
+
+  int i;
+  unsigned int pin;
+
+  /* Make sure that all accessed channels are in output mode */
+  for(i = 0; i < npulses; i++) {
+    meas_gpio_mode(CHTOGPIO(pulses[i].channel), 2);
+    meas_gpio_write(CHTOGPIO(pulses[i].channel), 0);
+  }
+
+  meas_gpio_interrupt(0); /* Disable interrupts */
+
+  for(i = 0; i < npulses; i++) {
+    pin = CHTOGPIO(pulses[i].channel);
+    meas_gpio_write(pin, 1);
+    meas_gpio_timer(pulses[i].length);
+    meas_gpio_write(pin, 0);
+    meas_gpio_timer(pulses[i].delay);
+  }
+
+  meas_gpio_interrupt(1); /* Enable interrupts */
+}
+
+#endif /* RPI */
