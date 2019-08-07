17008cf92fd8d9eaae7ac54a112aa0ed9f0e96eb
Jussi Eloranta <eloranta@aa6kj.hopto.org>
2019-08-07 15:11:23 -0700

    gpio example

--- examples/gpio/gpio.c
+++ examples/gpio/gpio.c
@@ -6,11 +6,11 @@ int main(int argc, char **argv) {
   int i;
 
   meas_gpio_open();
-  meas_gpio_mode(27, 2); // output
-  meas_gpio_interrupt(0);
+  meas_gpio_mode(27, 2); // GPIO pin 27 (BCM) to output
+  meas_gpio_interrupt(0);  // Disable interrupts (computer will freeze while running)
   for (i = 0; i < 1000000; i++) {
-    meas_gpio_write(27, 0); meas_gpio_timer(10);
-    meas_gpio_write(27, 1); meas_gpio_timer(10);
+    meas_gpio_write(27, 0); meas_gpio_timer(10); // set 27 to low and delay for 10 microsec.
+    meas_gpio_write(27, 1); meas_gpio_timer(10); // set 27 to high and delay for 10 microsec.
   }
-  meas_gpio_interrupt(1);
+  meas_gpio_interrupt(1); // enable interrupts again
 }
