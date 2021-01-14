07c8514c888ac3aaf22ac0a4d3dba4b88534898a
Jussi Eloranta <eloranta@aa6kj.hopto.org>
2021-01-14 10:28:20 -0800

    Make 2nd multimeter optional in volt.c example. Small changes to etc/rc.local

--- examples/voltmeter/volt.c
+++ examples/voltmeter/volt.c
@@ -7,19 +7,20 @@
 #include <math.h>
 #include <meas/meas.h>
 
-#define HPA 13
-#define HPB 14
+#define HPA 22
+//#define HPB 14
 
 int main(int argc, char **argv) {
 
   double va, vb;
 
   meas_hp34401a_open(0, 0, HPA);
-  meas_hp34401a_open(1, 0, HPB);
+//  meas_hp34401a_open(1, 0, HPB);
   meas_misc_set_reftime();
   while(1) {
     va = meas_hp34401a_read_auto(0);
-    vb = meas_hp34401a_read_auto(1);
-    printf("%le %le %le\n", meas_misc_get_reftime(), va, vb);
+//    vb = meas_hp34401a_read_auto(1);
+//    printf("%le %le %le\n", meas_misc_get_reftime(), va, vb);
+    printf("%le %le\n", meas_misc_get_reftime(), va);
   }
 }
